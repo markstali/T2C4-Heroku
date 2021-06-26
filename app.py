e75b5058884d0c9228858c96812e2b64e6a5abbd
Marcos Mendes <mrenuncia@gmail.com>
2021-06-26 18:34:37 -0300

    INserindo Js na calculadora

--- app.py
+++ app.py
@@ -4,7 +4,45 @@ app = Flask(__name__)
 @app.route("/")
 
 def home():
-    return render_template("index.html")
+    personagens = [
+        {
+            'nome' : 'Homem de Ferro',
+            'imagem' : '\static\img\ironman.png',
+            'vida' : 7
+        },
+        {
+            'nome' : 'Capitão America',
+            'imagem' : '\static\img\Capitao-America.png',
+            'vida' : 5
+        },
+        {
+            'nome' : 'Hulk',
+            'imagem' :'\static\img\hulk.png',
+            'vida' : 10
+        }
+    ]
+    armas = [
+        {
+            'nome' : 'Escudo',
+            'imagem' : '\static\img\Shield.png',
+            'dano' : 20
+        },
+        {
+            'nome' : 'Manopla',
+            'imagem' : '\static\img\manopla.png',
+            'dano' : 50
+        },
+        {
+            'nome' : 'Soco',
+            'imagem' : '\static\img\socoHulk.png',
+            'dano' : 10
+        }
+    ]
+    return render_template(
+        "index.html",
+        personagens=personagens,
+        armas=armas
+        )
 
 if __name__=="__main__":
     app.run(debug=True)
---
+++ static/index.js
@@ -0,0 +1,51 @@
+// indica quais são meus inimigos e minhas armas
+const personagens = document.querySelectorAll('.botao-personagem');
+const armas = document.querySelectorAll('.botao-armas');
+
+// MANIPULAÇÃO DA ÁRVORE DOM DO HTML
+
+// ENTENDER QUE EU CLIQUEI EM UM INIMIGO, E SELECIONAR ESTE INIMIGO
+
+for(const i of personagens){
+    alert(`o botao foi clicado`)
+
+    i.addEventListener('click', function(){
+        
+        if(document.getElementsByClassName('personagem-selecionado').length < 1){
+            this.classList.add('personagem-selecionado')
+            vidaDoPersonagem = Number(this.getAttribute('data-vida'));
+        } else{
+            this.classList.remove('personagem-selecionado')
+        }
+    })
+}
+
+for(const a of armas){
+
+    a.addEventListener('click', function(){
+        if(document.getElementsByClassName('arma-selecionada').length < 1){
+            danoDaArma = Number(this.getAttribute('data-dano'));
+            this.classList.add('arma-selecionada')
+        } else {
+            this.classList.remove('arma-selecionada')
+        }
+    })
+}
+
+function calcularDano(){
+
+    danoTotal = danoDaArma + rolarOsDados();
+
+    if(danoTotal >= vidaDoInimigo){
+        alert(`O dano foi ${danoTotal} e a vida do inimigo era ${vidaDoInimigo}. Você matou!!`)
+    } else {
+        alert(`O dano foi ${danoTotal} e a vida do inimigo era ${vidaDoInimigo}. O inimigo sobreviveu!!`)
+    }
+}
+
+function rolarOsDados() {
+    const min = Math.ceil(1);
+    const max = Math.floor(10);
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
